ab4688fd9ffee3d8ac39647b6137576e1df44470
Jasmine Pettersson <jpetters@paul-f3Ar2s5.clusters.42paris.fr>
2023-09-28 16:59:09 +0200

    Pushing everything at the end of the piscine.

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpetters <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/10 11:29:59 by jpetters          #+#    #+#             */
+/*   Updated: 2023/09/20 15:15:36 by jpetters         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+	}
+	if (nb < 0 && nb != -2147483648)
+	{
+		ft_putchar('-');
+		nb = nb * (-1);
+	}	
+	if (nb >= 0 && nb <= 9)
+	{
+		ft_putchar(nb + 48);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
+
+/*#include <stdio.h>
+
+void	ft_putnbr(int nb);
+
+int		main(void)
+{
+	ft_putnbr(42);
+	printf("\n");
+	ft_putnbr(-42);
+	printf("\n");
+	ft_putnbr(-2147483648);
+	printf("\n");
+	ft_putnbr(2147483647);
+	printf("\n");
+}*/
---
+++ ex03/ft_atoi.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpetters <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 18:07:40 by jpetters          #+#    #+#             */
+/*   Updated: 2023/09/20 12:38:19 by jpetters         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>*/
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	s;
+	int	r;
+
+	i = 0;
+	s = 1;
+	r = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i++] == '-')
+			s = -s;
+	}
+	while (str[i] >= '0' && str[i] <= '9' && str[i] != '\0')
+	{
+		r = r * 10 + (str[i] - '0');
+		i++;
+	}
+	return (s * r);
+}
+
+/*int	main(void)
+{
+	printf("42:%d\n", ft_atoi("  \n  42t4457"));
+	printf("-42:%d\n", ft_atoi(" --+-42sfs:f545"));
+	printf("0:%d\n", ft_atoi("\0 1337"));
+	printf("0:%d\n", ft_atoi("-0"));
+	printf("0:%d\n", ft_atoi(" - 1 3 2 5 6 3 2 1 6 7"));
+	printf("-1325632167:%d\n", ft_atoi("-1325632167"));
+	printf("-100:%d\n", ft_atoi("-100"));
+	printf("min:%d\n", ft_atoi("\t---+2147483648"));
+	printf("max:%d\n", ft_atoi("\v2147483647"));
+}*/
